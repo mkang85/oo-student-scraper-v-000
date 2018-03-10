124630069b610b867c0cd8e4d5ba638384ecb38c
Michael Kang <michaelkang85@gmail.com>
2018-03-10 19:29:58 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ end
   def self.scrape_profile_page(profile_url)
     student_page = Nokogiri::HTML(open("#{profile_url}"))
     binding.pry
-    links = student_page(".social-icon-container a")
+    links = student_page.css(".social-icon-container a")
   end
 
 end
