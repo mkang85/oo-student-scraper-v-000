a264f155186b128eca894ca2fd368c1029fc6c2b
Michael Kang <michaelkang85@gmail.com>
2018-03-10 19:42:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,8 +33,8 @@ end
         profile_page[:blog] = link
       end
     end
-    #bio = student_page.css(".vitals-text-container").css(".profile-quote").text
-    #profile_page[:bio] = bio
+    #quote = student_page.css(".vitals-text-container").css(".profile-quote").text
+    #profile_page[:profile_quote] = quote
     profile_page
   end
 
