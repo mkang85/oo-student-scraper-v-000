9003950c4ac12c440a0b5d54048474784cf89117
Michael Kang <michaelkang85@gmail.com>
2018-03-10 20:17:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,25 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    new_student = @student_hash.each do |key, val|
+      self.send("#{key}=", val)
+    end
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
-    
+    (iterate over an array of hashes)
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
